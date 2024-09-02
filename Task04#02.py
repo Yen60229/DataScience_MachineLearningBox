1095f31200da444091a1571009fd26f87ac79609
Yen60229 <90028411+Yen60229@users.noreply.github.com>
2024-09-03 02:48:18 +0800

    Update Task04#02.py

--- Task04#02.py
+++ Task04#02.py
@@ -14,39 +14,66 @@ raw_data = {'first_name': ['Jason', np.nan, 'Tina', 'Jake', 'Amy'],
 df = pd.DataFrame(raw_data, columns = ['first_name', 'last_name', 'age', 'sex', 'preTestScore', 'postTestScore'])
 
 # Drop missing observations
-drop_na = df.dropna()
+df1 = df * 1
+drop_na = df1.dropna()
 conn = sqlite3.connect('hw#04-raw_data_drop_na.db')
 drop_na.to_sql('hw#04-raw_data_drop_na', conn, if_exists='replace', index=False)
 conn.close()
 
 #Drop rows where all cells in that row is NA
-drop_all_missing = df.dropna(thresh = 1)
+df2 = df * 1
+drop_all_missing = df2.dropna(thresh = 1)
 conn = sqlite3.connect('hw#04-raw_data_drop_all_missing.db')
 drop_all_missing.to_sql('hw#04-raw_data_drop_all_missing', conn, if_exists='replace', index=False)
 conn.close()
 
 #Create a new column full of missing values
-df['NaN'] = np.nan
+df3 = df * 1
+df3['NaN'] = np.nan
 conn = sqlite3.connect('hw#04-raw_data_with_nan.db')
-df.to_sql('hw#04-raw_data_with_nan', conn, if_exists='replace', index=False)
+df3.to_sql('hw#04-raw_data_with_nan', conn, if_exists='replace', index=False)
 conn.close()
 
 #Fill in missing data with zeros
-df.fillna(0, inplace = True)
+df4 = df3.fillna(0)
 conn = sqlite3.connect('hw#04-raw_data_fillna.db')
-df.to_sql('hw#04-raw_data__fillna', conn, if_exists='replace', index=False)
+df4.to_sql('hw#04-raw_data__fillna', conn, if_exists='replace', index=False)
 conn.close()
 
 #Fill in missing in preTestScore with the mean value of preTestScore
-df.preTestScore = df.preTestScore.fillna(df.preTestScore.mean())
+df5 = df4 * 1
+#布爾序列來篩選和過濾raw_data的NULL值
+filtered_df = df[df['preTestScore'].notna()]
+preTestScore_mean = filtered_df['preTestScore'].mean()
+preTestScore_na = df['preTestScore'].isna()
+
+for i in df['preTestScore'].index:
+        if preTestScore_na.loc[i]:
+             df5.loc[i, 'preTestScore'] =  preTestScore_mean
+             
 conn = sqlite3.connect('hw#04-raw_data_PreTestScore.db')
-df.to_sql('hw#04-raw_data__PreTestScore', conn, if_exists='replace', index=False)
+df5.to_sql('hw#04-raw_data__PreTestScore', conn, if_exists='replace', index=False)
 conn.close()
 
 #Fill in missing in postTestScore with each sex’s mean value of postTestScore
-df.postTestScore = df.groupby('sex').postTestScore.transform(lambda x : x.fillna(x.mean()))
+df6 = df5 * 1
+
+# 過濾掉 preTestScore 為 NaN 的行，只保留 sex 和 postTestScore 列
+filtered_df = df[df['preTestScore'].notna()][['sex', 'postTestScore']]
+
+grouped_scores = filtered_df.groupby('sex')['postTestScore'].sum()
+print(grouped_scores)  
+                   
+preTestScore_na = df['postTestScore'].isna()
+for i in df['postTestScore'].index:
+    if preTestScore_na.loc[i]:
+        if df.loc[i, 'sex'] == 'f':
+            df6.loc[i, 'postTestScore'] = grouped_scores['f']    
+        if df.loc[i, 'sex'] == 'm':
+            df6.loc[i, 'postTestScore'] = grouped_scores['m'] 
+  
 conn = sqlite3.connect('hw#04-raw_data_postTestScore.db')
-df.to_sql('hw#04-raw_data_postTestScore', conn, if_exists='replace', index=False)
+df6.to_sql('hw#04-raw_data_postTestScore', conn, if_exists='replace', index=False)
 conn.close()
 
 #Select some rows but ignore the missing data points
@@ -54,4 +81,3 @@ df = df.dropna(how='any')
 conn = sqlite3.connect('hw#04-raw_data_ignore_miss.db')
 df.to_sql('hw#04-raw_data_ignore_miss', conn, if_exists='replace', index=False)
 conn.close()
-
